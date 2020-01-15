80f3e8f9482c660c7d4c670df5a22d470b7ff23c
Brandon Navarro <55892338+kalani-nav@users.noreply.github.com>
2020-01-15 13:47:36 -0700

    in class changes to onClick event and others

--- src/App.js
+++ src/App.js
@@ -3,16 +3,40 @@ import Welcome from './Welcome'
 import './App.scss'
 
 export default function App() {
-    const [ point, setPoint ] = React.useState([0,0])
+    const [ point, setPoint ] = React.useState([0,0]) //state variable - whenever you mess with state, essentially the page will re-render
+    //const [ point, setPoint ] = [ 0,0 ] //regular variable
+    const [ visible , setVisible ] = React.useState(true)
 
+    let user = {
+        name: 'brandon',
+        password: 'password'
+    }
+
+/*------WAYS TO MAKE FUNCTIONS------
+    function add10(num) {
+        return num + 10
+    }
+
+    const add11 = function(num) {
+        return num + 11
+    }
+
+    const add12 = (num) => {
+        return num + 12
+    }
+*/
     return (
         <div
             className="w-100 h-100 position-relative"
             onMouseMove={e => {
+                //console.log(e)
                 setPoint([e.clientX, e.clientY])
             }}
+            onClick={e => {
+                setVisible(!visible)
+            }}
         >
-            <Welcome point={point} />
+            <Welcome point={point} user={user} visible={visible}/>
         </div>
-    )
+    ) 
 }
--- src/Welcome.js
+++ src/Welcome.js
@@ -2,16 +2,20 @@ import React from 'react'
 import { Alert } from 'react-bootstrap'
 
 export default function Welcome(props) {
-    return (
-        <Alert
-            variant="info"
-            className="d-inline-block position-absolute"
-            style={{
-                left: props.point[0],
-                top: props.point[1],
-            }}
-        >
-            Welcome to 413!
-        </Alert>
-    )
+    if (props.visible){
+        return (
+            <Alert
+                variant="info"
+                className="d-inline-block position-absolute"
+                style={{
+                    left: props.point[0],
+                    top: props.point[1],
+                }}
+            >
+                {props.user.name} Welcome to 413!
+            </Alert>
+        )
+    }else{
+        return null
+    }
 }
